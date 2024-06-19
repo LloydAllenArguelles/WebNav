0c8313f6506dbba213554ea47d2759dfabaa4dd2
LloydAllenArguelles <allen.arguelles27@gmail.com>
2024-06-20 00:39:53 +0800

    added chatbot button
    Changes :
    Relocated AI Button on Top into bottom right. Applied in all pages (Home, Events, Tour) except Login.

---
+++ public/assets/chatbot.js
@@ -0,0 +1,14 @@
+function navigateTo(page) {
+    alert('Navigating to ' + page);
+    // wala logic pa
+}
+
+function showModal() {
+    document.getElementById('cb-overlay').style.display = 'block';
+    document.getElementById('cb-modal').style.display = 'block';
+}
+
+function hideModal() {
+    document.getElementById('cb-overlay').style.display = 'none';
+    document.getElementById('cb-modal').style.display = 'none';
+}
