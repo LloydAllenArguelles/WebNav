008aba58962d2f0a123613428e8da236991d7088
LloydAllenArguelles <allen.arguelles27@gmail.com>
2024-07-12 01:15:01 +0800

    add

--- public/assets/tour/gca-data.js
+++ public/assets/tour/gca-data.js
@@ -648,7 +648,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
