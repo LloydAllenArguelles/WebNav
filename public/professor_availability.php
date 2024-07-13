19ecffd91cb8883f40a1d67205b8ff0747ba0803
Patrick Sevilla <102337806+KamikazeGoBoom@users.noreply.github.com>
2024-07-13 12:04:21 +0800

    updated redirect link

--- public/professor_availability.php
+++ public/professor_availability.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: index.html");
+    header("Location: index.php");
     exit();
 }
 
