c0033e61ec499d66dda50210eea2ccaad1813d3b
LloydAllenArguelles <147525139+LloydAllenArguelles@users.noreply.github.com>
2024-07-20 09:50:04 +0800

    removed info-hotspot text

--- public/assets/tour/index.js
+++ public/assets/tour/index.js
@@ -458,14 +458,9 @@
     header.appendChild(titleWrapper);
     header.appendChild(closeWrapper);
 
-    // Create text element.
-    var text = document.createElement('div');
-    text.classList.add('info-hotspot-text');
-    text.innerHTML = hotspot.text;
 
     // Place header and text into wrapper element.
     wrapper.appendChild(header);
-    wrapper.appendChild(text);
 
     // Create a modal for the hotspot content to appear on mobile mode.
     var modal = document.createElement('div');
