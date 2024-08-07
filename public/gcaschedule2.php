06747f7e82fead90f83353d0187fc321718abc26
LloydAllenArguelles <147525139+LloydAllenArguelles@users.noreply.github.com>
2024-08-07 13:12:15 +0800

    minorfix? pt.2

--- public/gcaschedule2.php
+++ public/gcaschedule2.php
@@ -492,7 +492,7 @@ $is_admin = isset($_SESSION['role']) && $_SESSION['role'] === 'Admin';
 
     <script> const selectedDate = "<?php echo $selected_date; ?>"; </script>
     <script src="assets/js/buttons.js"></script>
-    <script src="assets/js/calendargca.js"></script>
+    <script src="assets/js/calendar.js"></script>
 
     <script>
     document.getElementById('show-occupied-schedules').addEventListener('click', function() {
